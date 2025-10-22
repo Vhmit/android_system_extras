d0fc9ebdaf4b7d0546cb2246fe11ec26d284d003
Vhmit <viktor.9630@protonmail.com>
2025-10-22 08:47:30 -0300

    extras: Enable configs for camera aux
    * way to select if the device has aux camera
    
    Change-Id: Ie7c10752b17377bafc323e8aee3a6345f678a6e0

---
+++ configs/GrapheneCam.mk
@@ -0,0 +1,6 @@
+PRODUCT_PACKAGES += GrapheneCam
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.vendor.camera.privapp.list=app.grapheneos.camera \
+    persist.camera.privapp.list=app.grapheneos.camera \
+    vendor.camera.aux.packagelist=app.grapheneos.camera 
