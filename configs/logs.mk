2b4977c4b02d83fe45311446fc5034c7c90998fd
Vhmit <viktor.9630@protonmail.com>
2025-10-21 22:57:05 -0300

    extras: include properties for logcat
    * include it to the device tree every time you need to take logs.
    
    Change-Id: Ibbc17b97066f30187d946d08cd7da49c640a452f

---
+++ configs/logs.mk
@@ -0,0 +1,8 @@
+# ADB properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1 \
+    persist.sys.usb.config=adb \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1
